6d349aea8550b3fbbbf8fb59e7e8448236478457
BBlu91 <trippelz@gmail.com>
2026-01-28 03:45:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def followup():
         recommendations=recs
     )
 
-    answer = ai_agent.generate_followup(followup_context)
+    answer = ai_agent.generate_followup(followup_context, followup_type)
 
     return jsonify({
         "success": True,
@@ -145,4 +145,5 @@ def get_values():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
